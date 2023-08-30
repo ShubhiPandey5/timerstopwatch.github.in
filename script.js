9af33324eca44330fe27034766bab230a8af3d7f
Apoorva <138895138+ShubhiPandey5@users.noreply.github.com>
2023-08-30 12:16:51 +0530

    tswatch

---
+++ script.js
@@ -0,0 +1,86 @@
+const playButton = document.getElementsByClassName("play")[0];
+const lapButton = document.getElementsByClassName("lap")[0];
+const resetButton = document.getElementsByClassName("reset")[0];
+const minute = document.getElementsByClassName("minute")[0];
+const second = document.getElementsByClassName("sec")[0];
+const centiSecond = document.getElementsByClassName("msec")[0];
+const laps = document.getElementsByClassName("laps")[0];
+
+let isPlay = false
+let secCounter = 0;
+let min;
+let minCounter = 0;
+let sec;
+let centisec;
+let centiCounter = 0;
+let isReset = false;
+let lapitem = 0;
+
+const toggleButton = () => {
+    lapButton.classList.remove("hidden");
+    resetButton.classList.remove("hidden");
+}
+
+const play = () =>{
+    if (!isPlay && !isReset){
+        playButton.innerHTML = 'Pause';
+        min = setInterval(() =>{
+                minute.innerHTML = ++minCounter;
+        }, 60*1000);
+        sec = setInterval(() =>{
+            if(secCounter === 60){
+                secCounter = 0;
+            }
+                second.innerHTML = ++secCounter;
+        }, 1000);
+        centiSec = setInterval(() =>{
+            if(centiCounter === 100){
+                centiCounter = 0;
+            }
+                centiSecond.innerHTML = ++centiCounter;
+        }, 10);
+        isPlay = true;
+        isReset = true;
+    }
+    else{
+        playButton.innerHTML = 'Play';
+        clearInterval(min);
+        clearInterval(sec);
+        clearInterval(centiSec);
+        isPlay = false;
+        isReset = false;
+    }
+    toggleButton();
+}
+
+const reset = () => {
+    isReset = true;
+    play();
+    lapButton.classList.add("hidden");
+    resetButton.classList.add("hidden");
+    second.innerHTML = '&nbsp;0 :'
+    minute.innerHTML = '0 :';
+    centiSecond.innerHTML = '&nbsp;0';
+}
+
+const lap =() => {
+    const li = document.createElement("li");
+    const number = document.createElement("span");
+    const timeStamp = document.createElement("span");
+
+    li.setAttribute("class","lap-item");
+    number.setAttribute("class","number");
+    timeStamp.setAttribute("class","time-stamp");
+
+    number.innerText = '#${++lapitem}'
+
+    timeStamp.innerHTML = '${minCounter} : ${secCounter} : ${centiCount}';
+
+    li.append(number , timeStamp);
+    laps.append(li);
+
+}
+
+playButton.addEventListener("click",play);
+resetButton.addEventListener('click',reset);
+lapButton.addEventListener("click",lap);
